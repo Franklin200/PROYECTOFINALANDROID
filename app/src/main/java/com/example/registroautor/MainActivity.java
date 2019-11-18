cfd525471cc985cfbace42e2e3299d5714a654d6
gissela2326 <gisselamargarita2623@gmal.com>
2019-11-17 18:21:36 -0600

    revise errores en codigo

--- app/src/main/java/com/example/registroautor/MainActivity.java
+++ app/src/main/java/com/example/registroautor/MainActivity.java
@@ -7,7 +7,6 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
@@ -15,7 +14,6 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
